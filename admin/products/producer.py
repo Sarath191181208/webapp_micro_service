82679b07539267943c13868269a10cabf8746fd1
Sarath191181208 <vssarathc04@gmail.com>
2024-05-31 13:41:00 +0530

    event publisher for flask app

---
+++ admin/products/producer.py
@@ -0,0 +1,14 @@
+import json
+import pika
+
+RABBIT_MQ_URL = "amqps://aygsuifi:CJLlLZquYmEdibi0jMExkEtq8i0S46fX@puffin.rmq2.cloudamqp.com/aygsuifi"
+
+conn = pika.BlockingConnection(pika.URLParameters(RABBIT_MQ_URL))
+channel = conn.channel()
+
+
+def publish(method, body):
+    props = pika.BasicProperties(method)
+    channel.basic_publish(
+        exchange="", routing_key="main", body=json.dumps(body), properties=props
+    )
