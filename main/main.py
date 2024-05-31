b2e01377ac4060e6d71c97510214e574c348dc41
Sarath191181208 <vssarathc04@gmail.com>
2024-05-31 13:40:11 +0530

    type hinting classes

--- main/main.py
+++ main/main.py
@@ -16,20 +16,16 @@ db = SQLAlchemy(app)
 
 @dataclass
 class Product(db.Model):
-    id: int
-    title: str
-    image: str
-
-    id = db.Column(db.Integer, primary_key=True, autoincrement=False)
-    title = db.Column(db.String(200))
-    image = db.Column(db.String(200))
+    id: int = db.Column(db.Integer, primary_key=True, autoincrement=False)
+    title: str = db.Column(db.String(200))
+    image: str = db.Column(db.String(200))
 
 
 @dataclass
 class ProductUser(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    user_id = db.Column(db.Integer)
-    product_id = db.Column(db.Integer)
+    id: int = db.Column(db.Integer, primary_key=True)
+    user_id: int = db.Column(db.Integer)
+    product_id: int = db.Column(db.Integer)
 
     UniqueConstraint("user_id", "product_id", name="user_product_unique")
 
