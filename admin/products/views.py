6b4f1cac7e9f1181910f0b9fc3fee27ec95ac951
Sarath191181208 <vssarathc04@gmail.com>
2024-05-31 13:42:21 +0530

    + events are published to queue

--- admin/products/views.py
+++ admin/products/views.py
@@ -6,7 +6,8 @@ from rest_framework.response import Response
 from rest_framework.views import APIView, status
 
 from .models import Product, User
-from .serializers import ProductSerializer, UserSerializer
+from .serializers import ProductSerializer
+from .producer import publish
 
 class ProductViewSet(viewsets.ViewSet):
     def list(self, _: Request):
@@ -24,6 +25,7 @@ class ProductViewSet(viewsets.ViewSet):
         """
         ser = ProductSerializer(data=req.data) # Deserialize the request data
         ser.is_valid(raise_exception=True) # Validate the data
+        publish('product_created', ser.data) # Sending the event to rabbit mq
         ser.save() # Save the data to the database
         return Response(ser.data, status=status.HTTP_201_CREATED)
 
@@ -43,6 +45,7 @@ class ProductViewSet(viewsets.ViewSet):
         serializer = ProductSerializer(instance=product, data=req.data) # Deserialize the request data
         serializer.is_valid(raise_exception=True) # Validate the data
         serializer.save() # Save the data to the database
+        publish('product_updated', ser.data) # Sending the event to rabbit mq
         return Response(serializer.data, status=status.HTTP_201_CREATED) # Return the serialized product
 
     def destroy(self, _: Request, primary_key: str | None = None): 
@@ -51,6 +54,7 @@ class ProductViewSet(viewsets.ViewSet):
         """
         product = Product.objects.get(id=primary_key) # Get the product from the database
         product.delete() # Delete the product from the database
+        publish('product_deleted', primary_key) # Sending the event to rabbit mq
         return Response(status=status.HTTP_204_NO_CONTENT) # Return a 204 status code
 
 class UserAPIView(APIView):
