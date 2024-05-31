bd41d28f844b33ed2c5bb79c8a80f193bb21a2b4
Sarath191181208 <vssarathc04@gmail.com>
2024-05-31 13:42:09 +0530

    consumer write to db

--- main/consumer.py
+++ main/consumer.py
@@ -1,4 +1,8 @@
+import json
+
 import pika
+
+from main import Product, db, app
 
 RABBIT_MQ_URL = "amqps://aygsuifi:CJLlLZquYmEdibi0jMExkEtq8i0S46fX@puffin.rmq2.cloudamqp.com/aygsuifi"
 QUEUE = "main"
@@ -9,15 +13,27 @@ channel.queue_declare(queue=QUEUE)
 
 
 def callback(ch, method, props, body):
-    print("Recived in admin: ")
+    print("Received in main")
     print(body)
+    data = json.loads(body)
+    print(data)
+    with app.app_context():
+        if props.content_type == "product_created":
+            product = Product(id=data["id"], title=data["name"], image=data["image"])
+            db.session.add(product)
+        elif props.content_type == "product_updated":
+            product = Product.query.get(data["id"])
+            if product is None:
+                return 
+            product.title = data["title"]
+            product.image = data["image"]
+        elif props.content_type == "product_deleted":
+            product = Product.query.get(data["id"])
+            db.session.delete(product)
+        db.session.commit()
 
-    with open("admin.log", "a") as f:
-        f.write(str(body))
-        f.write("\n")
 
-
-channel.basic_consume(queue=QUEUE, on_message_callback=callback)
+channel.basic_consume(queue=QUEUE, on_message_callback=callback, auto_ack=True)
 
 print("Started consuming...")
 
